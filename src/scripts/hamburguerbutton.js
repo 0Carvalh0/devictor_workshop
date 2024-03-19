b551bfe07cad6449ba564fb54f1a7c0480b2d0b9
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-03-19 20:27:09 -0300

    Organizei e arrumei estilização do header

--- src/scripts/hamburguerbutton.js
+++ src/scripts/hamburguerbutton.js
@@ -4,7 +4,9 @@ const navbar = document.getElementById("navbar");
 btn__Menu.addEventListener("click", () => {
   if (navbar.style.top < "0px") {
     navbar.style.top = "0px";
+    btn__Menu.classList.replace("fa-bars", "fa-xmark");
   } else {
     navbar.style.top = "-300px";
+    btn__Menu.classList.replace("fa-xmark", "fa-bars");
   }
 });
