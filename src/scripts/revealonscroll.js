893d11f0ead3eeed8c7ccd27b2134748e6f68840
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-03-23 22:28:06 -0300

    scroll reveal apply

---
+++ src/scripts/revealonscroll.js
@@ -0,0 +1,137 @@
+window.revelar = ScrollReveal({ reset: true });
+
+// TÍTULOS
+
+revelar.reveal(".title", {
+  duration: 1500,
+  distance: "50px",
+  delay: 1000,
+});
+
+// TOPO SITE
+
+revelar.reveal(".welcome__aboutme-text", {
+  duration: 1500,
+  distance: "50px",
+  origin: "left",
+});
+
+revelar.reveal(".welcome__efeito-imagem", {
+  duration: 1500,
+  distance: "50px",
+  delay: 500,
+});
+
+// PROJETOS
+
+revelar.reveal("#projects", {
+  duration: 1500,
+  distance: "50px",
+  delay: 500,
+});
+
+// HABILIDADES
+
+revelar.reveal(".skill1", {
+  duration: 1500,
+  distance: "50px",
+  delay: 500,
+});
+
+revelar.reveal(".skill2", {
+  duration: 1500,
+  distance: "50px",
+  delay: 1000,
+});
+
+revelar.reveal(".skill3", {
+  duration: 1500,
+  distance: "50px",
+  delay: 1500,
+});
+
+revelar.reveal(".skill4", {
+  duration: 1500,
+  distance: "50px",
+  delay: 2000,
+});
+
+revelar.reveal(".skill5", {
+  duration: 1500,
+  distance: "50px",
+  delay: 2500,
+});
+
+revelar.reveal(".skill6", {
+  duration: 1500,
+  distance: "50px",
+  delay: 3000,
+});
+
+revelar.reveal(".skill7", {
+  duration: 1500,
+  distance: "50px",
+  delay: 3500,
+});
+
+revelar.reveal(".skill8", {
+  duration: 1500,
+  distance: "50px",
+  delay: 4000,
+});
+
+revelar.reveal(".skill9", {
+  duration: 1500,
+  distance: "50px",
+  delay: 4500,
+});
+
+revelar.reveal(".skill10", {
+  duration: 1500,
+  distance: "50px",
+  delay: 5000,
+});
+
+revelar.reveal(".skill11", {
+  duration: 1500,
+  distance: "50px",
+  delay: 5500,
+});
+
+revelar.reveal(".skill12", {
+  duration: 1500,
+  distance: "50px",
+  delay: 6000,
+});
+
+// FOOTER
+
+revelar.reveal("#inome", {
+  duration: 1500,
+  distance: "50px",
+  delay: 500,
+});
+
+revelar.reveal("#imail", {
+  duration: 1500,
+  distance: "50px",
+  delay: 1000,
+});
+
+revelar.reveal("#itel", {
+  duration: 1500,
+  distance: "50px",
+  delay: 1500,
+});
+
+revelar.reveal("#iassunto", {
+  duration: 1500,
+  distance: "50px",
+  delay: 2000,
+});
+
+revelar.reveal("#imensagem", {
+  duration: 1500,
+  distance: "20px",
+  delay: 2500,
+});
