6928e2a1686d5eec6839cea976e098e7ca96ab83
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-03-23 23:22:48 -0300

    update scroll reveal fast

--- src/scripts/revealonscroll.js
+++ src/scripts/revealonscroll.js
@@ -5,7 +5,7 @@ window.revelar = ScrollReveal({ reset: true });
 revelar.reveal(".title", {
   duration: 1500,
   distance: "50px",
-  delay: 1000,
+  easing: "ease",
 });
 
 // TOPO SITE
@@ -14,94 +14,137 @@ revelar.reveal(".welcome__aboutme-text", {
   duration: 1500,
   distance: "50px",
   origin: "left",
+  easing: "ease",
 });
 
 revelar.reveal(".welcome__efeito-imagem", {
   duration: 1500,
   distance: "50px",
   delay: 500,
+  easing: "ease",
 });
 
 // PROJETOS
 
-revelar.reveal("#projects", {
+revelar.reveal(".projeto1", {
   duration: 1500,
   distance: "50px",
   delay: 500,
+  easing: "ease",
+});
+
+revelar.reveal(".projeto2", {
+  duration: 1500,
+  distance: "50px",
+  delay: 1000,
+  easing: "ease",
+});
+
+revelar.reveal(".projeto3", {
+  duration: 1500,
+  distance: "50px",
+  delay: 1500,
+  easing: "ease",
+});
+
+revelar.reveal(".projeto4", {
+  duration: 1500,
+  distance: "50px",
+  delay: 2000,
+  easing: "ease",
+});
+
+revelar.reveal(".projeto5", {
+  duration: 1500,
+  distance: "50px",
+  delay: 2500,
+  easing: "ease",
 });
 
 // HABILIDADES
 
 revelar.reveal(".skill1", {
-  duration: 1500,
+  duration: 800,
   distance: "50px",
   delay: 500,
+  easing: "ease",
 });
 
 revelar.reveal(".skill2", {
-  duration: 1500,
+  duration: 800,
   distance: "50px",
-  delay: 1000,
+  delay: 800,
+  easing: "ease",
 });
 
 revelar.reveal(".skill3", {
-  duration: 1500,
+  duration: 800,
   distance: "50px",
-  delay: 1500,
+  delay: 1200,
+  easing: "ease",
 });
 
 revelar.reveal(".skill4", {
-  duration: 1500,
+  duration: 800,
   distance: "50px",
-  delay: 2000,
+  delay: 1500,
+  easing: "ease",
 });
 
 revelar.reveal(".skill5", {
-  duration: 1500,
+  duration: 800,
   distance: "50px",
-  delay: 2500,
+  delay: 1800,
+  easing: "ease",
 });
 
 revelar.reveal(".skill6", {
-  duration: 1500,
+  duration: 800,
   distance: "50px",
-  delay: 3000,
+  delay: 2200,
+  easing: "ease",
 });
 
 revelar.reveal(".skill7", {
-  duration: 1500,
+  duration: 800,
   distance: "50px",
-  delay: 3500,
+  delay: 2500,
+  easing: "ease",
 });
 
 revelar.reveal(".skill8", {
-  duration: 1500,
+  duration: 800,
   distance: "50px",
-  delay: 4000,
+  delay: 2800,
+  easing: "ease",
 });
 
 revelar.reveal(".skill9", {
-  duration: 1500,
+  duration: 800,
   distance: "50px",
-  delay: 4500,
+  delay: 3200,
+  easing: "ease",
 });
 
 revelar.reveal(".skill10", {
-  duration: 1500,
+  duration: 800,
   distance: "50px",
-  delay: 5000,
+  delay: 3500,
+  easing: "ease",
 });
 
 revelar.reveal(".skill11", {
-  duration: 1500,
+  duration: 800,
   distance: "50px",
-  delay: 5500,
+  delay: 3800,
+  easing: "ease",
 });
 
 revelar.reveal(".skill12", {
-  duration: 1500,
+  duration: 800,
   distance: "50px",
-  delay: 6000,
+  delay: 4200,
+  easing: "ease",
 });
 
 // FOOTER
@@ -110,28 +153,33 @@ revelar.reveal("#inome", {
   duration: 1500,
   distance: "50px",
   delay: 500,
+  easing: "ease",
 });
 
 revelar.reveal("#imail", {
   duration: 1500,
   distance: "50px",
   delay: 1000,
+  easing: "ease",
 });
 
 revelar.reveal("#itel", {
   duration: 1500,
   distance: "50px",
   delay: 1500,
+  easing: "ease",
 });
 
 revelar.reveal("#iassunto", {
   duration: 1500,
   distance: "50px",
   delay: 2000,
+  easing: "ease",
 });
 
 revelar.reveal("#imensagem", {
   duration: 1500,
   distance: "20px",
   delay: 2500,
+  easing: "ease",
 });
