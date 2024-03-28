28754f4a31757479b34de3ba26fff5645a49cd8a
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-03-28 14:17:55 -0300

    feat: criei a lista de projetos

---
+++ src/scripts/projectsScript.js
@@ -0,0 +1,42 @@
+const projectsList = [
+  {
+    name: "Blink",
+    thumbnailSrc: "./src/assets/images/projetos/Blink.jpg",
+    description:
+      "Blink é uma rede social que funciona de maneira rápida, seus posts só são visíveis para você e partir do momento em que você sai todas suas informações serão perdidas e você tera que começar tudo de novo.",
+    urlProject: "https://blink-social.netlify.app/",
+    urlRepository: "https://github.com/0Carvalh0/projeto-blink",
+  },
+  {
+    name: "Te Salvando",
+    thumbnailSrc: "./src/assets/images/projetos/tesalvando.jpg",
+    description:
+      "Site para ajuda nos estudos para vestibulares. Com esse site você poderá: Ver conteúdos que precisam ser estudados, Ver dicas de como ir bem na prova, Ver datas de provas, Ler um jornal sobre as notícias que podem cair.",
+    urlProject: "https://0carvalh0.github.io/projeto-te-salvando.com/",
+    urlRepository: "https://github.com/0Carvalh0/projeto-te-salvando.com",
+  },
+  {
+    name: "CalculaJão",
+    thumbnailSrc: "./src/assets/images/projetos/calculajao.jpg",
+    description:
+      "Meu primeiro projeto em desenvolvimento front-end, calculadora com diversos cálculos utilizando HTML5, CSS3 e JavaScript.",
+    urlProject: "https://0carvalh0.github.io/calculajao/index.html",
+    urlRepository: "https://github.com/0Carvalh0/calculajao",
+  },
+  {
+    name: "Spotify Clone",
+    thumbnailSrc: "./src/assets/images/projetos/spotifyclone.png",
+    description:
+      "Projeto criado na imersão front-end da Alura, replicando um clone do Spotify usando o React",
+    urlProject: "https://spotifywithreactcarvalho.netlify.app/",
+    urlRepository: "https://github.com/0Carvalh0/spotify-react",
+  },
+  {
+    name: "Tela de Login",
+    thumbnailSrc: "./src/assets/images/projetos/telalogin.png",
+    description:
+      "Projeto front-end criado durante o curso do Curso em Vídeo do Gustavo Guanabara para treinar responsividade.",
+    urlProject: "https://0carvalh0.github.io/projeto-login/",
+    urlRepository: "https://github.com/0Carvalh0/projeto-login",
+  },
+];
