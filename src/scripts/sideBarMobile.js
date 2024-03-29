70bff202b7faf87254aabc038385ee9cd911eba9
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-03-28 22:10:30 -0300

    feat: arrumei o layout para os dispositivos mobile

--- src/scripts/sideBarMobile.js
+++ src/scripts/sideBarMobile.js
@@ -4,11 +4,11 @@ const btnSideBar = document.getElementById("sideBar-btnHamburguer"),
 // Abrir SideBar com botão
 
 btnSideBar.addEventListener("click", () => {
-  if (sideBarMenu.style.top === "-1080px") {
+  if (sideBarMenu.style.top <= "-2000px") {
     sideBarMenu.style.top = "0px";
     btnSideBar.classList.replace("fa-bars", "fa-xmark");
   } else {
-    sideBarMenu.style.top = "-1080px";
+    sideBarMenu.style.top = "-2000px";
     btnSideBar.classList.replace("fa-xmark", "fa-bars");
   }
 });
