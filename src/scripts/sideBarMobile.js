164044dbc32f137833e298ddff67c65f765974a5
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-03-31 10:36:37 -0300

    fix: menu hamburguer fechando e abrindo normalmente

--- src/scripts/sideBarMobile.js
+++ src/scripts/sideBarMobile.js
@@ -1,16 +1,25 @@
 const sideBarMenu = document.getElementById("sideBar");
 const btnSideBar = document.getElementById("sideBar-btnHamburguer");
+const linkSection = document.querySelectorAll(".btnMenu-sideBar");
 
 // Abrir SideBar com botão
 
 btnSideBar.addEventListener("click", () => {
   if (sideBarMenu.classList.contains("activeHamburguer")) {
-    sideBarMenu.style.top = "0px";
-    btnSideBar.classList.replace("fa-bars", "fa-xmark");
-    sideBarMenu.classList.remove("activeHamburguer")
-  } else {
     sideBarMenu.style.top = "-2000px";
     btnSideBar.classList.replace("fa-xmark", "fa-bars");
-    sideBarMenu.classList.add("activeHamburguer")
+    sideBarMenu.classList.remove("activeHamburguer");
+  } else {
+    sideBarMenu.style.top = "0px";
+    btnSideBar.classList.replace("fa-bars", "fa-xmark");
+    sideBarMenu.classList.add("activeHamburguer");
   }
 });
+
+linkSection.forEach((link) => {
+  link.addEventListener("click", () => {
+    btnSideBar.click();
+  });
+});
+
+console.log(linkSection)
