fa0f433aac618bd944921c01f59fca4421beb03a
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-03-30 21:05:08 -0300

    fix: menu hamburguer não abrindo

--- src/scripts/sideBarMobile.js
+++ src/scripts/sideBarMobile.js
@@ -1,14 +1,16 @@
-const btnSideBar = document.getElementById("sideBar-btnHamburguer"),
-  sideBarMenu = document.getElementById("sideBar");
+const sideBarMenu = document.getElementById("sideBar");
+const btnSideBar = document.getElementById("sideBar-btnHamburguer");
 
 // Abrir SideBar com botão
 
 btnSideBar.addEventListener("click", () => {
-  if (sideBarMenu.style.top <= "-2000px") {
+  if (sideBarMenu.classList.contains("activeHamburguer")) {
     sideBarMenu.style.top = "0px";
     btnSideBar.classList.replace("fa-bars", "fa-xmark");
+    sideBarMenu.classList.remove("activeHamburguer")
   } else {
     sideBarMenu.style.top = "-2000px";
     btnSideBar.classList.replace("fa-xmark", "fa-bars");
+    sideBarMenu.classList.add("activeHamburguer")
   }
 });
