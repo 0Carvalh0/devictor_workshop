bbfe493f9ddb5ab4f2cd7cd89bfe1bbd245d8613
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-04-24 22:35:09 -0300

    feat: adicionei as tecnologias utilizadas em cada projeto

--- src/scripts/projectsScript.jsx
+++ src/scripts/projectsScript.jsx
@@ -22,26 +22,26 @@ const projectsList = [
     thumbnailSrc: "./src/assets/images/projetos/calculajao.jpg",
     description:
       "Meu primeiro projeto em desenvolvimento front-end, calculadora com diversos cálculos utilizando HTML5, CSS3 e JavaScript.",
-      technologies: ["HTML5", "CSS3", "JavaScript"],
-      urlProject: "https://0carvalh0.github.io/calculajao/index.html",
-      urlRepository: "https://github.com/0Carvalh0/calculajao",
-    },
-    {
-      name: "Spotify Clone",
-      thumbnailSrc: "./src/assets/images/projetos/spotifyclone.png",
-      description:
+    technologies: ["HTML5", "CSS3", "JavaScript"],
+    urlProject: "https://0carvalh0.github.io/calculajao/index.html",
+    urlRepository: "https://github.com/0Carvalh0/calculajao",
+  },
+  {
+    name: "Spotify Clone",
+    thumbnailSrc: "./src/assets/images/projetos/spotifyclone.png",
+    description:
       "Projeto criado na imersão front-end da Alura, replicando um clone do Spotify usando o React",
-      technologies: ["React", "HTML5", "CSS3", "JavaScript"],
-      urlProject: "https://spotifywithreactcarvalho.netlify.app/",
-      urlRepository: "https://github.com/0Carvalh0/spotify-react",
-    },
-    {
-      name: "Tela de Login",
-      thumbnailSrc: "./src/assets/images/projetos/telalogin.png",
-      description:
+    technologies: ["React", "HTML5", "CSS3", "JavaScript"],
+    urlProject: "https://spotifywithreactcarvalho.netlify.app/",
+    urlRepository: "https://github.com/0Carvalh0/spotify-react",
+  },
+  {
+    name: "Tela de Login",
+    thumbnailSrc: "./src/assets/images/projetos/telalogin.png",
+    description:
       "Projeto front-end criado durante o curso do Curso em Vídeo do Gustavo Guanabara para treinar responsividade.",
-      technologies: ["HTML5", "CSS3"],
-      urlProject: "https://0carvalh0.github.io/projeto-login/",
+    technologies: ["HTML5", "CSS3"],
+    urlProject: "https://0carvalh0.github.io/projeto-login/",
     urlRepository: "https://github.com/0Carvalh0/projeto-login",
   },
 ];
@@ -54,27 +54,36 @@ function automaticProjectGenerator() {
     <section class="projectsList__Project projectCard" id="project${
       indexProject + 1
     }">
-      <img
-        src=${projectData.thumbnailSrc}
-        alt="Thumbnail do projeto"
-        class="projectsList__ProjectThumbnail"/>
-      <h2 class="mainSection__ProjectTitle">${projectData.name}</h2>
-      <p class="mainSection__ProjectDescription">
-        ${projectData.description}
-      </p>
-      <section class="mainSection__ProjectForMore">
+    <img
+    src=${projectData.thumbnailSrc}
+    alt="Thumbnail do projeto"
+    class="projectsList__ProjectThumbnail"/>
+    <h2 class="mainSection__ProjectTitle">${projectData.name}</h2>
+    <section class="projectsList__ProjectTechnologies" id="projectTechnologies${indexProject}">
+    </section>
+    <p class="mainSection__ProjectDescription">
+    ${projectData.description}
+    </p>
+    <section class="mainSection__ProjectForMore">
         <a href=${
           projectData.urlProject
         } target="_blank" class="mainSection__ProjectButton">Ver Projeto</a>
         <a href=${
           projectData.urlRepository
         } target="_blank" class="mainSection__ProjectButton">Ver Repositório</a>
-      </section>
-    </section>
-    `;
+        </section>
+        </section>
+        `;
 
     sectionInsert.innerHTML += projectStructure;
+
+    const projectTechnologiesSection = document.querySelector(
+      `#projectTechnologies${indexProject}`
+    );
+    projectData.technologies.forEach((technology) => {
+      const technologyIcon = `<img src="./src/assets/icons/${technology}.png" class="projectsList__TechnologyIcon" alt="ícone ${technology}" />`;
+      projectTechnologiesSection.innerHTML += technologyIcon;
+    });
   });
 }
-
 automaticProjectGenerator();
