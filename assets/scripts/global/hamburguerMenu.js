c1c6fd6092ed6d5da83f73e1eada1477da330c71
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-05-03 15:00:57 -0300

    feat: botão menu hamburguer abrindo e fechando corretamente

--- assets/scripts/global/hamburguerMenu.js
+++ assets/scripts/global/hamburguerMenu.js
@@ -1,12 +1,25 @@
 const buttonHamburguer = document.querySelector("#header__ButtonHamburguer");
-const buttonHamburguerClose = document.querySelector("#header__ButtonHamburguerClose")
+const buttonHamburguerClose = document.querySelector(
+  "#header__ButtonHamburguerClose"
+);
 const navList = document.querySelector("#header__Nav");
+const buttonSectionLink = document.querySelectorAll(".header__ButtonSection");
 
 buttonHamburguer.addEventListener("click", () => {
-  if (!buttonHamburguer.classList.contains("active") || !buttonHamburguerClose.classList.contains("active")) {
+  buttonHamburguer.classList.toggle("active");
+  toogleHamburguer();
+});
+
+buttonSectionLink.forEach((buttonSection) => {
+  buttonSection.addEventListener("click", () => {
+    buttonHamburguer.click();
+  });
+});
+
+function toogleHamburguer() {
+  if (!buttonHamburguer.classList.contains("active")) {
     navList.style.top = "0px";
   } else {
     navList.style.top = "-500px";
   }
-  buttonHamburguer.classList.toggle("active");
-});
+}
