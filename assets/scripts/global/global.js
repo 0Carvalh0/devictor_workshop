480df3b951b1232cabb1ed388d0b144826789e85
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-06-17 20:42:15 -0300

    fix: arrumei o carrosel

--- assets/scripts/global/global.js
+++ assets/scripts/global/global.js
@@ -27,12 +27,8 @@ inputPhone.addEventListener("keypress", () => {
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
-  spaceBetween: 30,
+  spaceBetween: 20,
   loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
