f8b4290f0630ea0076dd67ceb3fd17b4c529bc8f
Joao Victor <joctor.carvalho2004@gmail.com>
2025-07-25 22:42:38 -0300

    feat: organizacao dos projetos

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,73 +1,73 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const themeToggle = document.querySelector(".header__theme-toggle");
-  const body = document.body;
+    const themeToggle = document.querySelector(".header__theme-toggle");
+    const body = document.body;
 
-  // Funcionalidade de alternância de tema
-  themeToggle.addEventListener("click", () => {
-    body.classList.toggle("page--light-theme");
-    updateThemeIcon();
-    localStorage.setItem(
-      "theme",
-      body.classList.contains("page--light-theme") ? "light" : "dark"
-    );
-  });
+    // Funcionalidade de alternância de tema
+    themeToggle.addEventListener("click", () => {
+        body.classList.toggle("page--light-theme");
+        updateThemeIcon();
+        localStorage.setItem(
+            "theme",
+            body.classList.contains("page--light-theme") ? "light" : "dark"
+        );
+    });
 
-  // Verificar preferência de tema salva
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme === "light") {
-    body.classList.add("page--light-theme");
-    updateThemeIcon();
-  }
-
-  function updateThemeIcon() {
-    const sunIcon = themeToggle.querySelector(".fa-sun");
-    const moonIcon = themeToggle.querySelector(".fa-moon");
-    if (body.classList.contains("page--light-theme")) {
-      sunIcon.style.display = "inline-block";
-      moonIcon.style.display = "none";
-
-      document.querySelector(".header__logo").src =
-        "./assets/images/logos/Devictor_LogoPreta.png";
-    } else {
-      sunIcon.style.display = "none";
-      moonIcon.style.display = "inline-block";
-
-      document.querySelector(".header__logo").src =
-        "./assets/images/logos/Devictor_LogoBranca.png";
+    // Verificar preferência de tema salva
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "light") {
+        body.classList.add("page--light-theme");
+        updateThemeIcon();
     }
-  }
 
-  // Efeito de digitação na seção hero
-  let options = {
-    strings: [
-      "Desenvolvedor Front-end",
-      "Especialista em UI/UX",
-      "Entusiasta de JavaScript",
-    ],
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 1500,
-    loop: true,
-  };
+    function updateThemeIcon() {
+        const sunIcon = themeToggle.querySelector(".fa-sun");
+        const moonIcon = themeToggle.querySelector(".fa-moon");
+        if (body.classList.contains("page--light-theme")) {
+            sunIcon.style.display = "inline-block";
+            moonIcon.style.display = "none";
 
-  let typed = new Typed("#typing-text", options);
+            document.querySelector(".header__logo").src =
+                "./assets/images/logos/Devictor_LogoPreta.png";
+        } else {
+            sunIcon.style.display = "none";
+            moonIcon.style.display = "inline-block";
 
-  // Modal de agradecimento
-  const form = document.querySelector("#contact-form");
-  const showModal = sessionStorage.getItem("showThankYouModal");
-  const closeButton = document.querySelector(".thank-youModal__button");
-  const modal = document.querySelector(".thank-youModal");
+            document.querySelector(".header__logo").src =
+                "./assets/images/logos/Devictor_LogoBranca.png";
+        }
+    }
 
-  form.addEventListener("submit", () => {
-    sessionStorage.setItem("showThankYouModal", "true");
-  });
+    // Efeito de digitação na seção hero
+    let options = {
+        strings: [
+            "Desenvolvedor Front-end",
+            "Especialista em UI/UX",
+            "Entusiasta de JavaScript",
+        ],
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 1500,
+        loop: true,
+    };
 
-  if (showModal === "true") {
-    modal.showModal();
-    sessionStorage.removeItem("showThankYouModal");
-  }
+    let typed = new Typed("#typing-text", options);
 
-  closeButton.addEventListener("click", () => {
-    modal.close();
-  });
+    // Modal de agradecimento
+    const form = document.querySelector("#contact-form");
+    const showModal = sessionStorage.getItem("showThankYouModal");
+    const closeButton = document.querySelector(".thank-youModal__button");
+    const modal = document.querySelector(".thank-youModal");
+
+    form.addEventListener("submit", () => {
+        sessionStorage.setItem("showThankYouModal", "true");
+    });
+
+    if (showModal === "true") {
+        modal.showModal();
+        sessionStorage.removeItem("showThankYouModal");
+    }
+
+    closeButton.addEventListener("click", () => {
+        modal.close();
+    });
 });
